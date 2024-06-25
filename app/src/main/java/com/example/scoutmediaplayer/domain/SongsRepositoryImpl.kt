bd00170b6b3f122619d4165cd1dc07c94d9339c7
Roman Nesterenko <nister.r@gmail.com>
2024-06-24 23:37:58 -0700

    Add button to play remote file

--- app/src/main/java/com/example/scoutmediaplayer/domain/SongsRepositoryImpl.kt
+++ app/src/main/java/com/example/scoutmediaplayer/domain/SongsRepositoryImpl.kt
@@ -19,57 +19,9 @@ class SongsRepositoryImpl(private val applicationContext: Context) : SongsReposi
         val list = ArrayList<Song>()
         list.add(
             Song(
-                "Stub Artist",
-                "Stub Title",
-                "1:1",
-                "https://github.com/rafaelreis-hotmart/Audio-Sample-files/raw/master/sample.mp3"
-            )
-        )
-        list.add(
-            Song(
-                "Stub Artist 2",
-                "Stub Title 2",
-                "22:22",
-                "https://github.com/rafaelreis-hotmart/Audio-Sample-files/raw/master/sample.mp3"
-            )
-        )
-        list.add(
-            Song(
-                "Stub Artist 3",
-                "Stub Title 3",
-                "33:33",
-                "https://github.com/rafaelreis-hotmart/Audio-Sample-files/raw/master/sample.mp3"
-            )
-        )
-        list.add(
-            Song(
-                "Stub Artist 4",
-                "Stub Title 4",
-                "4:4",
-                "https://github.com/rafaelreis-hotmart/Audio-Sample-files/raw/master/sample.mp3"
-            )
-        )
-        list.add(
-            Song(
-                "Stub Artist 5",
-                "Stub Title 5",
-                "5:5",
-                "https://github.com/rafaelreis-hotmart/Audio-Sample-files/raw/master/sample.mp3"
-            )
-        )
-        list.add(
-            Song(
-                "Stub Artist 6",
-                "Stub Title 6",
-                "6:6",
-                "https://github.com/rafaelreis-hotmart/Audio-Sample-files/raw/master/sample.mp3"
-            )
-        )
-        list.add(
-            Song(
-                "Stub Artist 7",
-                "Stub Title 7",
-                "7:7",
+                "Kai Engel",
+                "Blizzard (PON I)",
+                "03:00",
                 "https://github.com/rafaelreis-hotmart/Audio-Sample-files/raw/master/sample.mp3"
             )
         )
